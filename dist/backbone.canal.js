e09f1b438e38bd065856cbfbfc811ec088e6ff8a
Matt Morgan <mlmorg@gmail.com>
2013-03-17 13:14:35 -0400

    Setup route parameter parsing

--- dist/backbone.canal.js
+++ dist/backbone.canal.js
@@ -7,4 +7,49 @@
 
   "use strict";
 
+  // Alias libraries
+  var root = this;
+  var _ = root._;
+  var Backbone = root.Backbone;
+
+  // References for overridden methods
+  var _routeToRegExp = Backbone.Router.prototype._routeToRegExp;
+  var _extractParameters = Backbone.Router.prototype._extractParameters;
+
+  // Useful regex patters
+  var paramIdentifiers = /[\:\*]/g;
+  var paramNames = /[\:\*](\w+)/g;
+
+  // Extend Backbone.Router
+  // ----------------------
+  _.extend(Backbone.Router.prototype, {
+
+    _routeToRegExp: function (route) {
+      // Get route parameter names
+      var names = _.map(route.match(paramNames), function (name) {
+        return name.replace(paramIdentifiers, '');
+      });
+
+      // Create RegExp with original method
+      var regex = _routeToRegExp(route);
+
+      // Include paramter names on regex object
+      regex.names = names;
+      return regex;
+    },
+
+    _extractParameters: function (route, fragment) {
+      // Extract named/splat parameters into hash using original method
+      var params = {};
+      _.each(_extractParameters(route, fragment), function (param, i) {
+        // Set names according to the names array on the regex object
+        params[route.names[i]] = param;
+      });
+
+      // Return arguments array
+      return [params];
+    }
+
+  });
+
 }).call(this);
--- spec/backbone.canal.spec.js
+++ spec/backbone.canal.spec.js
@@ -1,4 +1,41 @@
 describe('backbone.canal.js', function () {
 
+  var Router, router, navigate;
+  var query = { q: 'joe' };
+
+  before(function () {
+    Router = Backbone.Router.extend({
+      routes: {
+        'search/:type': 'search'
+      },
+      search: sinon.spy()
+    });
+
+    router = new Router();
+    navigate = sinon.spy(router, 'navigate');
+
+    try {
+      Backbone.history.start({ pushState: false, silent: true });
+    } catch (e) {}
+  });
+
+  after(function () {
+    router.navigate('');
+    navigate.restore();
+  });
+
+  describe('when navigating to a route with named params', function () {
+
+    var type = 'name';
+
+    before(function () {
+      router.navigate('search/' + type, true);
+    });
+
+    it('should pass key/value pair object as argument', function () {
+      expect(router.search.args[0][0]).eql({ type: type });
+    });
+
+  });
 
 });
